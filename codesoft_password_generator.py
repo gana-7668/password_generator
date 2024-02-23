25999ced371a814b5905294c7f9aeb080e3028db
Ganesh Jadhav <136150686+gana-7668@users.noreply.github.com>
2024-02-23 22:24:05 +0530

    Add files via upload

---
+++ codesoft_password_generator.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+while True:
+    def generate_password(length=15):
+        characters= string.ascii_letters + string.digits + string.punctuation
+
+        password="".join(random.choice(characters) for _ in range(length))
+
+        return password
+
+    if __name__=="__main__":
+        password_length=15
+
+        try:
+            password_length=int(input("enter desired password length (defalt password length is 15)::"))
+
+        except:
+            print("invalid input. using default password length:")
+
+        if password_length<=0:
+            print("password length should be greater than 0. using default password length: ")
+            password_length=15
+
+        generate_password=generate_password(password_length)
+        print(generate_password)
+
+        
